0230e11e950e0e8a473ee6893cacb7bc9fba2e6d
vishvaops44 <80167936+vishvaops44@users.noreply.github.com>
2024-11-28 20:18:18 -0500

    Schema updates

--- modules/terraform-azurerm-postgres/main.tf
+++ modules/terraform-azurerm-postgres/main.tf
@@ -226,8 +226,9 @@ resource "aws_secretsmanager_secret_rotation" "this" {
 
 # Run the SQL script after DB instance creation
 resource "null_resource" "db_setup" {
-  count = !local.is_replica ? 1 : 0
+  #count = !local.is_replica ? 1 : 0
   #depends_on = [aws_db_instance.this[0]]
+  count = var.replicate_source_db == null ? 1 : 0
 
   provisioner "local-exec" {
     command = "psql -h ${aws_db_instance.this[0].endpoint} -U ${var.username} -d ${var.db_name} -f ./db_setup.sql"
